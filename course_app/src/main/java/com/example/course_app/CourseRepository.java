4de8e3dc2d58fc0131527f8b91f086f44952873b
francisPalen <rfrancispalen@gmail.com>
2026-02-24 11:43:29 +0000

    First commit

---
+++ course_app/src/main/java/com/example/course_app/CourseRepository.java
@@ -0,0 +1,8 @@
+package com.example.course_app;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface CourseRepository extends CrudRepository<Course, Long> {
+
+}
---
+++ course_app/src/main/java/com/example/course_app/LoadData.java
@@ -0,0 +1,30 @@
+package com.example.course_app;
+
+
+import org.springframework.boot.CommandLineRunner;
+
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+
+public class LoadData {
+
+
+    @Bean
+
+    CommandLineRunner initDatabase(CourseRepository repository) {
+
+        return args -> {
+
+            repository.save(new Course("Spring Boot Basics", "Mark"));
+
+            repository.save(new Course("REST APIs", "Alastair"));
+
+        };
+
+    }
+
+}
